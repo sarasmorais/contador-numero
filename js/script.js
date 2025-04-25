dca6a9db4b80fe4e88169225b938fd37000426ed
Sara Morais <sarasouza.m@gmail.com>
2025-04-25 16:42:23 -0300

    add arq

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function contar (){
+    const numero = number(document.getElementById('inputNumber').value);
+    let resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = "";
+
+    for(let i =1; i<= numero; i++){
+        resultado.innerHTML += i + " ";
+    }
+}
+
+let button = document.querySelector('button')
+button.addEventListener('click',contar)
+
